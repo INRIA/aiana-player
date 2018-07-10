43e7c3c91661af85b1a28ff57f12745ab9d75256
dashdashzako <d@dashdashzako.net>
2018-07-10 16:34:51 +0200

    Add i18n support

---
+++ src/IntlWrapper.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+import App from './App';
+
+import { addLocaleData, IntlProvider } from 'react-intl';
+import * as locale_en from 'react-intl/locale-data/en';
+import * as locale_fr from 'react-intl/locale-data/fr';
+import * as messages_fr from './translations/fr.json';
+
+interface IState {
+  language: string;
+}
+
+interface IProps {
+  readonly language?: string;
+}
+
+const defaultLanguage = 'en';
+
+const messages: any = {
+  fr: messages_fr
+};
+
+addLocaleData([...locale_en, ...locale_fr]);
+
+class IntlWrapper extends React.Component<IProps, IState> {
+  public static defaultProps = {
+    language: defaultLanguage
+  };
+
+  constructor(props: any) {
+    super(props);
+
+    const { language } = props;
+
+    this.state = { language };
+  }
+
+  public render(): JSX.Element {
+    const { language = defaultLanguage } = this.state;
+
+    return (
+      <IntlProvider locale={language} messages={messages[language]}>
+        <App />
+      </IntlProvider>
+    );
+  }
+}
+
+export default IntlWrapper;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+
 import './index.css';
+
+import IntlWrapper from './IntlWrapper';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<IntlWrapper />, document.getElementById('root'));
 registerServiceWorker();
