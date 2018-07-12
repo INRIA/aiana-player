296065f240e90ce773e37b95be56d9b8efd66534
dashdashzako <d@dashdashzako.net>
2018-07-12 11:04:56 +0200

    Rename Components directory

---
+++ src/components/Controls.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Controls extends React.Component {
+  public render() {
+    return (
+      <div className="aiana-controls">
+        <button className="aiana-controls-btn">
+          <span>Play/Pause</span>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Controls;
