fc294645d4a7ddd08657a315b98aba4b061368ce
dashdashzako <d@dashdashzako.net>
2018-08-22 15:00:37 +0200

    Add missing types, add storybook missing folder

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
+export const MEDIA_TYPE_VIDEO = 'video';
+
 export const DEFAULT_LANG = 'fr';
 export const DEFAULT_MEDIA_TYPE = 'video';
 export const DEFAULT_NATIVE_CONTROLS = true;
-
-export const MEDIA_TYPE_VIDEO = 'video';
