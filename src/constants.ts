b0fc97dfa5707f692eb2ca97f43fbc82aa332ef2
dashdashzako <d@dashdashzako.net>
2018-08-30 11:11:02 +0200

    Set native video player controls to false

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const MEDIA_TYPE_VIDEO = 'video';
 
 export const DEFAULT_LANG = 'fr';
 export const DEFAULT_MEDIA_TYPE = 'video';
-export const DEFAULT_NATIVE_CONTROLS = true;
+export const DEFAULT_NATIVE_CONTROLS = false;
