018881fff8ee7c6fea370a6cb8a0b9260a2c6b05
dashdashzako <d@dashdashzako.net>
2018-09-17 16:32:42 +0200

    Use custom volume component

--- src/components/buttons/VolumeSlider.tsx
+++ src/components/buttons/VolumeSlider.tsx
@@ -3,44 +3,189 @@ import { connect } from 'react-redux';
 import { requestChangeVolume } from '../../actions/player';
 import { IAianaState } from '../../reducers/index';
 import { IConnectedReduxProps } from '../../store/index';
+import { unitToPercent } from '../../utils/math';
+import styled from '../../utils/styled-components';
+import { injectFocusable } from './focusable';
 
-interface IProps {
+export interface IVolumeSliderProps {
   videoElement: HTMLVideoElement;
   volume: number;
 }
 
-class VolumeSlider extends React.Component<IProps & IConnectedReduxProps> {
-  private volumeInput = React.createRef<HTMLInputElement>();
+const StyledDiv = styled.div`
+  display: inline-block;
+  width: 4em;
+  height: 100%;
+  cursor: pointer;
+
+  .aip-volume-slider {
+    height: 100%;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    touch-action: none;
+  }
+
+  .aip-volume-slider-handle {
+    position: absolute;
+    top: calc(50% - 0.5em);
+    height: 1em;
+    width: 1em;
+    background-color: #fff;
+    border-radius: 0.5em;
+
+    &:before,
+    &:after {
+      content: ' ';
+      position: absolute;
+      display: block;
+      top: calc(50% - 2px);
+      height: 4px;
+      width: 5em;
+    }
+
+    &:before {
+      background-color: #fff;
+      right: 0.5em;
+    }
+
+    &:after {
+      background-color: hsla(0, 0%, 100%, 0.23);
+      left: 0.5em;
+    }
+  }
+`;
+
+class VolumeSlider extends React.Component<
+  IVolumeSliderProps & IConnectedReduxProps
+> {
+  public sliderPosition = 0;
+  public sliderWidth = 0;
+
+  public sliderRef = React.createRef<HTMLDivElement>();
+
+  public get slider() {
+    return this.sliderRef.current;
+  }
 
   public render() {
     const { volume } = this.props;
+    const volumePercents = 100 * volume;
+    const position = 0.75 * volumePercents;
 
     return (
-      <input
-        ref={this.volumeInput}
-        type="range"
-        step={1}
-        min={0}
-        max={100}
-        onChange={this.handleChangeVolume}
-        value={this.volumeToPercents(volume)}
-      />
+      <StyledDiv
+        className="aip-volume"
+        role="slider"
+        tabIndex={0}
+        aria-label="Volume level"
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-valuenow={volumePercents}
+        aria-valuetext="TODO valeur courante"
+        onKeyDown={this.keyDownHandler}
+      >
+        <div
+          ref={this.sliderRef}
+          className="aip-volume-slider"
+          onMouseDownCapture={this.mouseDownHandler}
+        >
+          <div
+            className="aip-volume-slider-handle"
+            style={{
+              left: `${position}%`
+            }}
+          />
+        </div>
+      </StyledDiv>
     );
   }
 
-  private handleChangeVolume = () => {
-    const { dispatch, videoElement } = this.props;
-    const newVolume = Number(this.volumeInput.current!.value) / 100;
+  private keyDownHandler = (evt: React.KeyboardEvent<HTMLDivElement>) => {
+    const { dispatch, videoElement, volume } = this.props;
 
-    dispatch(requestChangeVolume(videoElement, newVolume));
+    switch (evt.keyCode) {
+      case 39:
+        dispatch(
+          requestChangeVolume(
+            videoElement,
+            this.computeVolumeInRange(volume + 0.1)
+          )
+        );
+        break;
+      case 37:
+        dispatch(
+          requestChangeVolume(
+            videoElement,
+            this.computeVolumeInRange(volume - 0.1)
+          )
+        );
+        break;
+    }
   };
 
-  private volumeToPercents = (volume: number) => {
-    return volume * 100;
+  private mouseDownHandler = (evt: React.MouseEvent<HTMLDivElement>) => {
+    evt.preventDefault();
+    // recalculate slider element position to ensure no external
+    // event (such as fullscreen or window redimension) changed it.
+    const { left, width } = this.slider!.getBoundingClientRect();
+
+    this.sliderPosition = left;
+    this.sliderWidth = width;
+
+    // trigger first recomputation to simulate simple click.
+    this.diffPosition(this.sliderWidth, this.sliderPosition, evt.pageX);
+
+    document.addEventListener('mousemove', this.mouseMoveHandler, true);
+    document.addEventListener('mouseup', this.mouseUpHandler, true);
   };
+
+  private mouseUpHandler = () => {
+    document.removeEventListener('mousemove', this.mouseMoveHandler, true);
+    document.removeEventListener('mouseup', this.mouseUpHandler, true);
+  };
+
+  private mouseMoveHandler = (evt: MouseEvent) => {
+    this.diffPosition(this.sliderWidth, this.sliderPosition, evt.pageX);
+  };
+
+  private diffPosition = (
+    sliderWidth: number,
+    sliderX: number,
+    mouseX: number
+  ) => {
+    const { dispatch, videoElement, volume } = this.props;
+    const recordedDifference = mouseX - sliderX;
+    let positionDifference;
+
+    if (recordedDifference < 0) {
+      positionDifference = 0;
+    } else if (recordedDifference > this.sliderWidth) {
+      positionDifference = this.sliderWidth;
+    } else {
+      positionDifference = recordedDifference;
+    }
+
+    const newVolume = unitToPercent(positionDifference, sliderWidth) / 100;
+    if (newVolume !== volume) {
+      dispatch(requestChangeVolume(videoElement, newVolume));
+    }
+  };
+
+  private computeVolumeInRange(inputVolume: number) {
+    let outputVolume = inputVolume;
+
+    if (inputVolume < 0) {
+      outputVolume = 0;
+    } else if (inputVolume > 1) {
+      outputVolume = 1;
+    }
+
+    return outputVolume;
+  }
 }
 
 export default connect((state: IAianaState) => ({
   videoElement: state.player.videoElement,
   volume: state.player.volume
-}))(VolumeSlider);
+}))(injectFocusable(VolumeSlider));
