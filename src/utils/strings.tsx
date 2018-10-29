9d891d07a3d745eb09b08ba589ceb9affaead40c
dashdashzako <d@dashdashzako.net>
2018-10-29 17:45:43 +0100

    Remove headers ids to marked content

--- src/utils/strings.tsx
+++ src/utils/strings.tsx
@@ -1,6 +1,10 @@
 import * as marked from 'marked';
 import * as React from 'react';
 
+marked.setOptions({
+  headerIds: false
+});
+
 export function formatSubtitles(text: string): string[] {
   return text.split(/\n/).map((line) => line.trim());
 }
