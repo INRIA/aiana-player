84bcbd30b7cb042fee047d2f53aba0de2422e71c
dashdashzako <d@dashdashzako.net>
2018-11-12 15:40:32 +0100

    Update icons

---
+++ src/components/svg/Bookmark.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Bookmark: React.SFC = (props) => (
+  <svg viewBox="0 0 36 36" width="1em" height="1em" {...props}>
+    <path d="M11 8v22l7-5 7 5V8z" />
+  </svg>
+);
+
+export default Bookmark;
--- src/components/svg/BookmarkAdd.tsx
+++ src/components/svg/BookmarkAdd.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const BookmarkAdd: React.SFC = (props) => (
   <svg viewBox="0 0 36 36" width="1em" height="1em" {...props}>
-    <path d="M15 8q-3 0-3 3v17l7-5 7 5V11q0-3-3-3z" />
+    <path d="M11 8v22l7-5 7 5V8h-6v6h4v2h-4v4h-2v-4h-4v-2h4v-4h4V8z" />
   </svg>
 );
 
