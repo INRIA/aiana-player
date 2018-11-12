802c5a06f6d0a509ef0a291de7407122133245d6
dashdashzako <d@dashdashzako.net>
2018-11-12 15:41:01 +0100

    Create random color generator utility

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -12,3 +12,12 @@ export function hexToHsla(color: string, alpha: number): string {
   const [h, s, l] = convert.hex.hsl(color);
   return `hsla(${h}, ${s}%, ${l}%, ${alpha})`;
 }
+
+export function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    color = `${color}${letters[Math.floor(Math.random() * 16)]}`;
+  }
+  return `#${color}`;
+}
