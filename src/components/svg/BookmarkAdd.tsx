74b7ed72c9e7dda22d2da7802ecf7ac951f5e4e9
dashdashzako <d@dashdashzako.net>
2018-11-16 08:31:08 +0100

    Update add bookmark icon

--- src/components/svg/BookmarkAdd.tsx
+++ src/components/svg/BookmarkAdd.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 
 const BookmarkAdd: React.SFC = (props) => (
   <svg viewBox="0 0 36 36" width="1em" height="1em" {...props}>
-    <path d="M11 8v22l7-5 7 5V8h-6v6h4v2h-4v4h-2v-4h-4v-2h4v-4h4V8z" />
+    <path
+      d="M11 8v22l7-5 7 5V8zm5.5 3v3h-3v3h3v3h3v-3h3v-3h-3v-3z"
+      fillRule="evenodd"
+    />
   </svg>
 );
 
