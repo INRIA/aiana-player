065979c2ef0bfdfa47c35504e4c9eca073a21b6c
dashdashzako <d@dashdashzako.net>
2018-12-20 11:38:47 +0100

    Improve subtitles styles

--- src/components/subtitles/Styles.ts
+++ src/components/subtitles/Styles.ts
@@ -5,8 +5,6 @@ export const StyledSubtitles = styled.div`
   position: absolute;
   bottom: 6.5rem;
   left: 50%;
-  line-height: 1.5em;
-  font-size: 1.25em;
   text-align: center;
 
   pointer-events: none;
@@ -19,10 +17,15 @@ export const StyledSpan = styled.span`
   span {
     display: inline-block;
     padding: 0 0.3em;
+
     border-radius: 0.2em;
     color: ${(props) => props.theme.fg};
     background: ${(props) => hexToHsla(props.theme.bg, 0.9)};
 
+    line-height: 1.5em;
+    font-size: 1.25em;
+
     white-space: nowrap;
+    pointer-events: all;
   }
 `;
