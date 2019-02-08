3d3080ecefd4514cedfae5d8d33f84b4cefbd430
dashdashzako <d@dashdashzako.net>
2019-02-08 12:31:04 +0100

    Remove usage of JSX global namespace

--- src/utils/strings.tsx
+++ src/utils/strings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import marked from './marked';
 
 /**
@@ -26,12 +26,12 @@ function identity(input: string) {
 /**
  * Converts input string (markdown, HTML) a JSX element.
  */
-export function markdownToJSX(md: string): JSX.Element {
+export function markdownToJSX(md: string): ReactElement<any> {
   return unsafeJSX(marked)(md);
 }
 
 function unsafeJSX(adapter: Adapter = identity) {
-  return (content: string): JSX.Element => (
+  return (content: string): ReactElement<any> => (
     <div
       className="aip-marked"
       dangerouslySetInnerHTML={{ __html: adapter(content) }}
