96dbbeddbda64885347a469cf55d758772f30e0f
dashdashzako <d@dashdashzako.net>
2019-02-25 15:40:26 +0100

    Add previous/next chapter navigation components

---
+++ src/components/chapters/controls/NextChapter.tsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { WithTranslation, withTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import { seekNextChapter } from '../../../actions/chapters';
+import { IAianaState } from '../../../reducers';
+import { IRawChaptersTrack, isActiveTrack } from '../../../utils/media';
+import AssistiveText from '../../a11y/AssistiveText';
+import StyledButton from '../../styled/StyledButton';
+import StyledSvg from '../../styled/StyledSvg';
+import SkipNext from '../../svg/SkipNext';
+
+interface IStateProps {
+  chaptersTracks: IRawChaptersTrack[];
+  currentTime: number;
+  duration: number;
+  media?: HTMLMediaElement;
+}
+
+interface IDispatchProps {
+  seekNextChapter(media: HTMLMediaElement, from: number, to: number): void;
+}
+
+interface INextChapter extends IStateProps, IDispatchProps, WithTranslation {}
+
+const StyledSkipNext = StyledSvg.withComponent(SkipNext);
+
+// TODO: what to do when there is no chapters at all?
+// Disable the element? Just hide it?
+
+class NextChapter extends Component<INextChapter> {
+  render() {
+    return (
+      <StyledButton onClick={this.clickHandler} type="button">
+        <StyledSkipNext aria-hidden="true" />
+        <AssistiveText>
+          {this.props.t('controls.play_chapter_next')}
+        </AssistiveText>
+      </StyledButton>
+    );
+  }
+
+  clickHandler = () => {
+    const activeChaptersTrack = this.props.chaptersTracks.find(isActiveTrack);
+
+    if (!activeChaptersTrack) {
+      return;
+    }
+
+    const nextChapterCue = activeChaptersTrack.cues.find((cue) => {
+      return cue.startTime > this.props.currentTime;
+    });
+
+    // If last cue is already played, seek to the end of the media.
+    this.props.seekNextChapter(
+      this.props.media!,
+      this.props.currentTime,
+      nextChapterCue ? nextChapterCue.startTime : this.props.duration
+    );
+  };
+}
+
+function mapStateToProps(state: IAianaState) {
+  return {
+    chaptersTracks: state.chapters.chaptersTracks,
+    currentTime: state.player.currentTime,
+    duration: state.player.duration,
+    media: state.player.mediaElement
+  };
+}
+
+const mapDispatchToProps = {
+  seekNextChapter
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(NextChapter));
---
+++ src/components/chapters/controls/PreviousChapter.tsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { WithTranslation, withTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import { seekPreviousChapter } from '../../../actions/chapters';
+import { IAianaState } from '../../../reducers';
+import { IRawChaptersTrack, isActiveTrack } from '../../../utils/media';
+import AssistiveText from '../../a11y/AssistiveText';
+import StyledButton from '../../styled/StyledButton';
+import StyledSvg from '../../styled/StyledSvg';
+import SkipPrevious from '../../svg/SkipPrevious';
+
+interface IStateProps {
+  chaptersTracks: IRawChaptersTrack[];
+  currentTime: number;
+  media?: HTMLMediaElement;
+  seekThreshold: number;
+}
+
+interface IDispatchProps {
+  seekPreviousChapter(media: HTMLMediaElement, from: number, to: number): void;
+}
+
+interface IPreviousChapter
+  extends IStateProps,
+    IDispatchProps,
+    WithTranslation {}
+
+const StyledSkipPrevious = StyledSvg.withComponent(SkipPrevious);
+
+// TODO: what to do when there are no chapters at all?
+// Disable the element? Just hide it?
+
+class PreviousChapter extends Component<IPreviousChapter> {
+  render() {
+    return (
+      <StyledButton onClick={this.clickHandler} type="button">
+        <StyledSkipPrevious aria-hidden="true" />
+        <AssistiveText>
+          {this.props.t('controls.play_chapter_previous')}
+        </AssistiveText>
+      </StyledButton>
+    );
+  }
+
+  clickHandler = () => {
+    const activeChaptersTrack = this.props.chaptersTracks.find(isActiveTrack);
+
+    if (!activeChaptersTrack) {
+      return;
+    }
+
+    const previousChapterCue = [...activeChaptersTrack.cues]
+      .reverse()
+      .find((cue) => {
+        return (
+          cue.startTime < this.props.currentTime - this.props.seekThreshold
+        );
+      });
+
+    // If there is no cue found, seek to the beginning of the media.
+    this.props.seekPreviousChapter(
+      this.props.media!,
+      this.props.currentTime,
+      previousChapterCue ? previousChapterCue.startTime : 0
+    );
+  };
+}
+
+function mapStateToProps(state: IAianaState) {
+  return {
+    chaptersTracks: state.chapters.chaptersTracks,
+    currentTime: state.player.currentTime,
+    media: state.player.mediaElement,
+    seekThreshold: state.preferences.previousChapterSeekThreshold
+  };
+}
+
+const mapDispatchToProps = {
+  seekPreviousChapter
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(PreviousChapter));
