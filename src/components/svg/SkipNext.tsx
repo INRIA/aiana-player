60aff3bd0dee20eaf31527a6d5826600937fd772
dashdashzako <d@dashdashzako.net>
2019-02-25 16:03:35 +0100

    Update skip chapters icons

--- src/components/svg/SkipNext.tsx
+++ src/components/svg/SkipNext.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function SvgSkipNext(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 36 36" {...props}>
-      <path d="M6 9v18l10-9zM16 9v18l10-9zM26 9v18h4V9z" />
+      <path d="M7 10v16l10-8z" />
+      <path d="M16 10v16l10-8z" />
+      <path d="M25 10v16h4V10z" />
     </svg>
   );
 }
--- src/components/svg/SkipPrevious.tsx
+++ src/components/svg/SkipPrevious.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function SvgSkipPrevious(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 36 36" {...props}>
-      <path d="M30 27V9l-10 9zM20 27V9l-10 9zM10 27V9H6v18z" />
+      <path d="M29 26V10l-10 8z" />
+      <path d="M20 26V10l-10 8z" />
+      <path d="M11 26V10H7v16z" />
     </svg>
   );
 }
