16da8dfe94d11e6e199c7ee7bca29e3926a8d150
dashdashzako <d@dashdashzako.net>
2019-03-06 16:26:44 +0100

    Adjust slides navigation icons sizes

--- src/components/svg/ArrowBackward.tsx
+++ src/components/svg/ArrowBackward.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function SvgArrowBackward(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 36 36" {...props}>
-      <path d="M30 20v-4H10v4z" />
-      <path d="M8 20.828L5.172 18l12.02-12.02 2.829 2.828z" />
-      <path d="M5.172 18L8 15.172l12.02 12.02-2.828 2.829z" />
+      <path d="M28 20v-4H11v4z" />
+      <path d="M10 20.828L7.172 18 17.778 7.393l2.829 2.829z" />
+      <path d="M7.172 18L10 15.172l10.607 10.606-2.829 2.829z" />
     </svg>
   );
 }
--- src/components/svg/ArrowForward.tsx
+++ src/components/svg/ArrowForward.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function SvgArrowForward(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 36 36" {...props}>
-      <path d="M6 16v4h20v-4z" />
-      <path d="M28 15.172L30.828 18l-12.02 12.02-2.829-2.828z" />
-      <path d="M30.828 18L28 20.828 15.98 8.808l2.828-2.829z" />
+      <path d="M8 16v4h17v-4z" />
+      <path d="M26 15.172L28.828 18 18.222 28.607l-2.829-2.829z" />
+      <path d="M28.828 18L26 20.828 15.393 10.222l2.829-2.829z" />
     </svg>
   );
 }
