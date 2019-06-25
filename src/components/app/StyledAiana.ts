26836906a719782a2552ae62286a4bd3b10f6d53
dashdashzako <d@dashdashzako.net>
2019-06-25 08:27:58 +0200

    Improve content display when JS is disabled

--- src/components/app/StyledAiana.ts
+++ src/components/app/StyledAiana.ts
@@ -1,20 +1,8 @@
 import styled from '../../utils/styled-components';
 
 const StyledAiana = styled.div`
-  display: block;
-  width: 800px;
-  /*
-    The wanted canvas ratio is 16/9. To achieve that with a fixed width at
-    800px, height should be 450px plus the height of the controls.
-  */
-  height: 546px;
-  box-sizing: border-box;
   background-color: ${(props) => props.theme.bg};
   color: ${(props) => props.theme.fg};
-  font-size: 1em;
-  font-family: system, sans-serif;
-
-  -webkit-font-smoothing: antialiased;
 
   &.inactive {
     &,
@@ -22,21 +10,6 @@ const StyledAiana = styled.div`
       cursor: none;
     }
   }
-
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-
-  /* [data-focus-visible-added] {
-    box-shadow: inset 0 0 2px 2px ${(props) => props.theme.focus};
-    outline: none;
-  } */
-
-  /* *:focus:not([data-focus-visible-added]) {
-    outline: none;
-  } */
 
   select {
     font-family: inherit;
