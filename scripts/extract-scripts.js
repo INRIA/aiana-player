92c7f1846f8347d9f26cd7bd150a91b15088daeb
dashdashzako <d@dashdashzako.net>
2019-10-10 16:40:31 +0200

    Update travis config and release scripts

--- scripts/extract-scripts.js
+++ scripts/extract-scripts.js
@@ -3,31 +3,52 @@ const Parser = require('htmlparser2').Parser;
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-const pwd = require('pwd');
 
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const releaseDirname = process.argv.slice(2)[0];
 
-const currentDir = pwd.currentDir();
+const currentDir = process.cwd();
 const releaseDir = path.join(currentDir, releaseDirname);
-
-const parser = new Parser({
-  onopentag(name, attribs) {
-    if (name === 'script' && /\/static\/js/.test(attribs.src)) {
-      const fp = path.join(__dirname, 'build', attribs.src);
-      const filename = path.basename(fp);
-      fs.rename(fp, path.join(releaseDir, filename), (err) => {
-        throw err;
-      });
-    }
-  }
-});
 
 readFile('build/index.html')
   .then((content) => {
+    const filenames = [];
+
+    const parser = new Parser({
+      onopentag(name, attribs) {
+        if (name === 'script' && /\/static\/js/.test(attribs.src)) {
+          const fp = path.join(currentDir, 'build', attribs.src);
+          const filename = path.basename(attribs.src);
+          filenames.push(filename);
+
+          console.log(`Moving ${filename} to release directory...`);
+
+          fs.rename(fp, path.join(releaseDir, filename), (err) => {
+            if (err) {
+              throw err;
+            }
+
+            console.log(`Successfully moved ${filename} to release directory.`);
+          });
+        }
+      }
+    });
+
     parser.write(content);
+
+    return filenames;
+  })
+  .then((filenames) => {
+    const manifestPath = path.join(releaseDir, 'manifest.yml');
+    const manifestContent = filenames.reduce((filename) => `- ${filename}\n`);
+
+    return writeFile(manifestPath, manifestContent);
+  })
+  .then(() => {
+    console.log('Manifest written to release directory.\nAll done.');
   })
   .catch((err) => {
-    console.log('OOOPS', err);
+    throw err;
   });
